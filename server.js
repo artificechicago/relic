19aee60e0e39f2acbecd5df68c1d45a7cb066735
James E Crooks <jec@alpha.(none)>
2014-05-07 21:41:55 -0500

    Switching to direct mailer mode, because Gmail is a pain.  But it works!  Now on to more important things.

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var app = express();
 var connect = require('connect');
 var nodemailer = require('nodemailer');
 
-var auth = require('./auth.json'); //This should be provided by the production server
-
 // Set up app
 app.use(express.static(__dirname + '/website'));
 
@@ -17,24 +15,23 @@ app.post('/sign-up.html', connect.bodyParser(), function(req, res) {
     var comment = req.body.comment;
     console.log(req.body); //Debugging command
 
-    var smtpTransport = nodemailer.createTransport("SMTP", {service: "Gmail", auth:auth});
-
+    var mail = nodemailer.createTransport();
     mailOptions = {
-	from: "Artifice Sign-Up Notice <artificechicago@gmail.com>",
+	from: "Artifice Sign-Up Notice <no-reply@artificechicago.org>",
 	to: ["James Crooks <crooks1379@gmail.com>", "Ashley Lane <alane0101@gmail.com>", "Artifice NFP <artificechicago@gmail.com>"],
-	subject: "[ARTIFICE SIGN-UP] Test Notice",
+	subject: "[ARTIFICE SIGN-UP] [" + type + "] " + name + " has signed up",
 	text: "There has been a new sign-up with the following \n" + "Name: " +
 	    name + "\n" + "Email: " + email + "\n" + "Tel: " + tel
 	    + "\n" + "Type: " + type + "\n" + "Comments: " + comment
     };
 
-    smtpTransport.sendMail(mailOptions, function (error, response) {
+    mail.sendMail(mailOptions, function (error, response) {
 	if (error) {
 	    console.log(error);
 	} else {
 	    console.log("Message send: " + response.message);
 	}
-	smtpTransport.close();
+//	smtpTransport.close();
     });
 
     res.redirect('/');
