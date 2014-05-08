dc7bace319b3878d138800ee550e43a513b1e9fd
James E Crooks <jec@alpha.(none)>
2014-05-07 20:13:19 -0500

    Fixed mail script to/from fields

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.post('/sign-up.html', connect.bodyParser(), function(req, res) {
     var smtpTransport = nodemailer.createTransport("SMTP", {service: "Gmail", auth:auth});
 
     mailOptions = {
-	from: "Artifice Sign-Up Notice <crooks1379@gmail.com>",
-	to: "James Crooks <crooks1379@gmail.com>",
+	from: "Artifice Sign-Up Notice <artificechicago@gmail.com>",
+	to: ["James Crooks <crooks1379@gmail.com>", "Ashley Lane <alane0101@gmail.com>", "Artifice NFP <artificechicago@gmail.com>"],
 	subject: "[ARTIFICE SIGN-UP] Test Notice",
 	text: "There has been a new sign-up with the following \n" + "Name: " +
 	    name + "\n" + "Email: " + email + "\n" + "Tel: " + tel
@@ -40,4 +40,4 @@ app.post('/sign-up.html', connect.bodyParser(), function(req, res) {
     res.redirect('/');
 });
 
-app.listen(8080);
+app.listen(80);
